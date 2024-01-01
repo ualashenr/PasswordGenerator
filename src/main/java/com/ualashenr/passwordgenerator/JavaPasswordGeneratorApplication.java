6bc77469028c6b713965ca23318bfa5d7fba0513
Ualas Henrique <itsfake087@hotmail.com>
2023-12-31 21:36:12 -0300

    Project created

---
+++ src/main/java/com/ualashenr/passwordgenerator/JavaPasswordGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ualashenr.passwordgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPasswordGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPasswordGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ualashenr/passwordgenerator/JavaPasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ualashenr.passwordgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPasswordGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
